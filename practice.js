94aa5a79d14ffa287b8c85f3a165fc3f5f47a261
getahsaan <getahsaan@gmail.com>
2023-03-11 11:30:26 +0600

    done upto 3

---
+++ practice.js
@@ -0,0 +1,21 @@
+// ১. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও 
+
+// const getUserInput = prompt();
+// const userInput = parseFloat(getUserInput);
+// const result = userInput + 200;
+// alert(result);
+
+// ২. ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও
+
+// const userInput = confirm('Are you see location of your website?')
+// if (userInput === true) {
+//     console.log(location.href);
+// } else {
+//     console.log('Ok, as your wish');
+// }
+
+// ৩. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা ইংরেজিতে ৫ থেকে ৭ লাইনের মধ্যে লিখে ফেলো। 
+
+// HTTP cookies (also called web cookies, Internet cookies, browser cookies, or simply cookies) are small blocks of data created by a web server while a user is browsing a website and placed on the user's computer or other device by the user's web browser. Cookies are placed on the device used to access a website, and more than one cookie may be placed on a user's device during a session.
+
+// Cookies serve useful and sometimes essential functions on the web. They enable web servers to store stateful information (such as items added in the shopping cart in an online store) on the user's device or to track the user's browsing activity (including clicking particular buttons, logging in, or recording which pages were visited in the past).[1] They can also be used to save for subsequent use information that the user previously entered into form fields, such as names, addresses, passwords, and payment card numbers.
